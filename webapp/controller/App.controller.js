8edbf275124851d801249da2c8b743082919bc58
mrFleshka <mrFleshka@gmail.com>
2019-06-04 14:45:44 +0300

    Step 7: JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,21 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
     "use strict";
     // You can use return or extend declaration path (first param).
     return Controller.extend("sapui5.walkthrough.controller.App", {
+        onInit : function () {
+            // set data model on view
+            var oData = {
+                recipient : {
+                    name : "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
         onShowHello : function () {
             MessageToast.show("Hello World");
         }
